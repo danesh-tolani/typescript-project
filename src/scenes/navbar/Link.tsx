5564764e6366a94bd0c98f70b25d43d2d743fd50
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-03-17 17:27:51 +0000

    added enums

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,14 +1,15 @@
 import AnchorLink from "react-anchor-link-smooth-scroll"
+import { SelectedPage } from "@/shared/types"
 
 type Props = {
   page: string,
-  selectedPage: string,
-  setSelectedPage: (value: string) => void
+  selectedPage: SelectedPage,
+  setSelectedPage: (value: SelectedPage) => void
 }
 
 const Link = ({page, selectedPage, setSelectedPage}: Props) => {
 
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "") // making all lower case and removing any spaces
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage// making all lower case and removing any spaces
 
   return (
     <AnchorLink
