f5b8148299ed260a030136e8ae58e8977df3b017
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-03-18 05:53:19 +0000

    added mobile responsiveness in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/scenes/navbar"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SelectedPage } from "@/shared/types"
 
 
@@ -7,10 +7,27 @@ import { SelectedPage } from "@/shared/types"
 function App() {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) {  // you are at the top of the page
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      } 
+      if(window.scrollY !== 0) {
+        setIsTopOfPage(false)
+      }
+    }
+    
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)  
+  }, [])
+  
 
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} isTopOfPage={isTopOfPage}/>
     </div>
   )
 }
